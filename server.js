fa23369bea261c861b32acf6d05080ed6f42f061
Aram48 <aram.gabrielyann88@gmail.com>
2024-06-10 15:59:26 +0400

    change

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import { createServer } from "http";
+const PORT = 3000;
 
 const server = createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello, World!")
 });
 
-server.listen(3000);
+server.listen(PORT);
